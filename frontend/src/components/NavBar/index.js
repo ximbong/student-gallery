18fce376db5ceb128c2a0e7004d3a3f8a8900f61
Hai Truong <ximbong91023@yahoo.com>
2018-07-23 14:18:44 +0300

    Add NavBar component

---
+++ frontend/src/components/NavBar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../../assets/big-logo.png";
+
+import "./index.css";
+
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <img src={logo} alt="" />
+    </div>
+  );
+};
+
+export default NavBar;
