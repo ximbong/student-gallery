ed835a30aab542d32936098b7979886c14312607
ximbong91023 <ximbong91023@yahoo.com>
2018-07-24 16:45:48 +0300

    More routing

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -5,12 +5,26 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/big-logo.png";
 import "./index.css";
 
-const NavBar = () => {
+const NavBar = props => {
+  const pathName = props.location.pathname;
+
+  const rightSideButton =
+    pathName !== "/new" ? (
+      <Link to="/new">
+        <button>Add new student</button>
+      </Link>
+    ) : (
+      <Link to="/">
+        <button>Back to homepage</button>
+      </Link>
+    );
+
   return (
     <div className="navBar">
       <Link to="/">
         <img src={logo} alt="" />
       </Link>
+      {rightSideButton}
     </div>
   );
 };
